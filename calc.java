14e94ce9a91e849c25677e779a06c96e77ed5c62
honeypots1 <59510798+honeypots1@users.noreply.github.com>
2020-03-10 08:55:55 -0400

    wd

---
+++ calc.java
@@ -0,0 +1,14 @@
+function calculates() {
+        var order = document.getElementById('washingmachine');
+        document.cookie = 'form/title=My Name is Richard; expires=Tue, 29 Aug 2017 12:00:01 UTC'
+        setTimeout(function(){
+          window.location = "./solution.html";
+        }, 1000);
+
+
+    }
+
+function calculate() {
+    window.location = "./solution.html";
+
+}
