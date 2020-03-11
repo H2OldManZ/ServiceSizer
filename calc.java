c693223af8a406fc35bdac5b15a227fff8392610
honeypots1 <59510798+honeypots1@users.noreply.github.com>
2020-03-11 16:07:30 -0400

    wed

--- calc.java
+++ calc.java
@@ -250,20 +250,42 @@ function calculatemaxheadloss() {
       var customerpress = parseInt((document.getElementById('customerpress')).value);
       var elevdiff = parseInt((document.getElementById('elevdiff')).value);
       var calcmaxheadloss = (elevdiff) + 2.31*(supplypress-customerpress);
+      var calcmaxheadlosspsi = (elevdiff) + (supplypress-customerpress);
+
       if (isNaN(calcmaxheadloss) != true){
         document.getElementById('maxheadloss').value = calcmaxheadloss;
+        document.getElementById('maxheadlosspsi').value = calcmaxheadlosspsi;
+      }
+      if (calcmaxheadloss < 0){
+        calcmaxheadloss = "Not Possible";
+        document.getElementById('maxheadloss').value = calcmaxheadloss;
+        document.getElementById('maxheadlosspsi').value = calcmaxheadloss;
       }
 
 }
 
 function clearmaxheadloss() {
       document.getElementById('supplypress').value = "";
-      document.getElementById('customerpress').value = "";
-      document.getElementById('elevdiff').value = "";
+      document.getElementById('customerpress').value = "20";
+      document.getElementById('elevdiff').value = "0";
       document.getElementById('maxheadloss').value = "";
 
 }
 
-function calcvelocity() {
+function calcvelocitypipe() {
       (((4 * gpm / 448.8) / (3.14159 * 8)) ^ 0.5) * 12
+}
+
+function calculatepipelengthheadloss() {
+      var pipelength = parseInt((document.getElementById('pipelength')).value);
+      var calcgpm = parseInt((document.getElementById('calcgpm')).value);
+      var pipediameter = parseInt((document.getElementById('pipediameter')).value);
+      var calcpipelengthheadloss = 0.002083 * pipelength * (100/130)**1.85 * (calcgpm**1.85 / pipediameter**4.8655);
+      document.getElementById('pipeheadloss').value = "";
+      document.getElementById('headlosspsi').value = "";
+      if (isNaN(calcpipelengthheadloss) != true){
+        document.getElementById('pipeheadloss').value = calcpipelengthheadloss;
+        document.getElementById('headlosspsi').value = calcpipelengthheadloss / 2.31 ;
+      }
+
 }
